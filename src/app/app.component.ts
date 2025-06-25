5f5d59b12298bb55ca6ad8669c0fbe568a0fb92b
Fariz <fariz.rizaldy@salt.co.id>
2025-06-25 10:06:14 +0700

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Animal } from '../model/animal.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'fif-june-angular';
+  title: string = 'fif-june-angular';
+  testVariable: string = 'something'
+  anotherVariabel: string = 'value pertama';
+  isApproved: boolean = true;
+  dp: number = 0;
+  fruit: string[] = []
+  animal: Array<Animal> = []
+  animals: Animal[] = [
+    {
+      name: 'badak',
+      age: 10
+    },
+    {
+      name: 'harimau'
+    }
+  ]
+
+
+ testFunction() {
+    const testVariable = 'test variable'
+    this.anotherVariabel = testVariable;
+  }
+
+  anotherFunction() {
+    let testVariable = 'test variable'
+    testVariable = 'lalal'
+    this.testVariable = 'someeee'
+  }
 }
